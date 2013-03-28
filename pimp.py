58be3cee3e6be3c9d5c4da8832284e4b61de0f7d
Augusto Roccasalva <augusto@rocctech.com.ar>
2013-03-28 04:14:00 -0300

    More work on process_sheet

--- pimp.py
+++ pimp.py
@@ -9,10 +9,25 @@
 
 from os import path
 from collections import defaultdict
-from xlrd import open_workbook, XL_CELL_TEXT
+from decimal import Decimal
+from xlrd import open_workbook
+from xlrd import XL_CELL_TEXT, XL_CELL_NUMBER
 from xlutils.copy import copy as copy_workbook
 
+#from nobix.db import setup_db, Session
+#from nobix.models import Articulo
+#from nobix.config import load_config
+
+q = Decimal('0.01')
+
 test_file = 'test_file/griferia.xls'
+
+
+def init_nobix_db():
+    config = load_config()
+    setup_db(config.database_uri)
+    session = Session()
+    return session
 
 
 def get_spec(sheet):
@@ -38,13 +53,42 @@ def get_spec(sheet):
     return dict(spec)
 
 
+def cast(cell):
+    if cell.ctype is XL_CELL_NUMBER:
+        if isinstance(cell.value, float):
+            return Decimal(str(cell.value)).quantize(q)
+        return int(cell.value)
+    elif cell.ctype is XL_CELL_TEXT:
+        return cell.value.strip()
+    return cell.value
+
+
 def process_sheet(sheet, spec):
+    ref_name = spec['ref'].keys()[0]
+    rev_col = spec['ref'][ref_name]
+    reads = list(spec['read'].iteritems())
+    updates = list(spec['update'].iteritems())
+
+    if 'status' in spec:
+        def log_status(msg, row):
+            pass
+    else:
+        def log_status(msg, row):
+            pass
+
     def ignore(cell):
         return cell.ctype == XL_CELL_TEXT and cell.value.startswith(u'!')
     for r in range(spec['startrow'], sheet.nrows):
-        if any(map(ignore, sheet.row(r))):
+        row = sheet.row(r)
+        if any(map(ignore, row)):
             continue
-
+        ref_val = cast(row[rev_col])
+        if ref_val:
+# 1st read fields
+# 2nd update fields
+            newval = dict([(k, cast(row[i])) for k, i in updates])
+# 3rd stamp status
+            print ref_val, newval
 
 if __name__ == '__main__':
     workbook = open_workbook(test_file, on_demand=True)
