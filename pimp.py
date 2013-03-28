26389b043ba67dc53c1d5aacbf68cccd0f6423ce
Augusto Roccasalva <augusto@rocctech.com.ar>
2013-03-28 11:44:53 -0300

    More work on process_sheet, writing out status field

--- pimp.py
+++ pimp.py
@@ -14,9 +14,11 @@ from xlrd import open_workbook
 from xlrd import XL_CELL_TEXT, XL_CELL_NUMBER
 from xlutils.copy import copy as copy_workbook
 
-#from nobix.db import setup_db, Session
-#from nobix.models import Articulo
-#from nobix.config import load_config
+from nobix.db import setup_db, Session
+from nobix.models import Articulo
+from nobix.config import load_config
+
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 
 q = Decimal('0.01')
 
@@ -63,7 +65,7 @@ def cast(cell):
     return cell.value
 
 
-def process_sheet(sheet, spec):
+def process_sheet(sheet, spec, outsheet, session):
     ref_name = spec['ref'].keys()[0]
     rev_col = spec['ref'][ref_name]
     reads = list(spec['read'].iteritems())
@@ -71,27 +73,48 @@ def process_sheet(sheet, spec):
 
     if 'status' in spec:
         def log_status(msg, row):
-            pass
+            outsheet.write(row, spec['status'], msg)
     else:
         def log_status(msg, row):
-            pass
+            print "%s (#%s)" % (msg, row)
 
     def ignore(cell):
         return cell.ctype == XL_CELL_TEXT and cell.value.startswith(u'!')
+
     for r in range(spec['startrow'], sheet.nrows):
         row = sheet.row(r)
         if any(map(ignore, row)):
             continue
+
         ref_val = cast(row[rev_col])
         if ref_val:
-# 1st read fields
-# 2nd update fields
-            newval = dict([(k, cast(row[i])) for k, i in updates])
-# 3rd stamp status
-            print ref_val, newval
+            # 0st retrieve article by ref
+            try:
+                art = session.query(Articulo).filter(getattr(Articulo, ref_name) == ref_val).one()
+            except NoResultFound:
+                log_status("ERR: No se encuentra articulo que cumpla con '%s==%s'" % (ref_name, ref_val), r)
+                continue
+            except MultipleResultsFound:
+                log_status("ERR: La condición '%s==%s' arroja multiples resultados" % (ref_name, rev_val), r)
+                continue
+            except Exception as e:
+                log_status("EXCEPTION: %s" % " ".join(e.args), r)
+                continue
+            # 1st read fields
+            # 2nd update fields
+            toupdate = dict([(k, cast(row[i])) for k, i in updates])
+            # 3rd stamp status
+            print ref_val, toupdate
 
 if __name__ == '__main__':
-    workbook = open_workbook(test_file, on_demand=True)
-    test_sheet = workbook.sheet_by_name(u'GRIFERIA')
+    sheet_name = u'GRIFERIA'
+    workbook = open_workbook(test_file, on_demand=True, formatting_info=True)
+    test_sheet = workbook.sheet_by_name(sheet_name)
+    sheet_index = workbook.sheet_names().index(sheet_name)
+    out_wb = copy_workbook(workbook)
+    out_sheet = out_wb.get_sheet(sheet_index)
     spec = get_spec(test_sheet)
-    process_sheet(test_sheet, spec)
+    print 'spec:', spec
+    session = init_nobix_db()
+    process_sheet(test_sheet, spec, out_sheet, session)
+    out_wb.save('test_output.xls')
